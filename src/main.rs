1be6864886de0e6787e3951880665fa01ec9527e
Ken Salter <ken@thesalters.net>
2025-01-23 11:51:19 -0600

    add unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+
 use chrono::{DateTime, Utc};
 use clap::Parser;
 use indicatif::ProgressBar;
 use md5::{self, Digest};
 use std::io::Read;
-use std::thread;
+use std::{fs, thread};
 use std::time::UNIX_EPOCH;
 use std::{collections::HashMap, time::Duration};
 
@@ -48,7 +49,13 @@ struct FileInfo {
 fn main() {
     let args = get_command_line_arguments();
 
-    start_search(&args);
+    match start_search(&args) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            std::process::exit(1);
+        }
+    }
 }
 
 fn get_command_line_arguments() -> Args {
@@ -64,14 +71,20 @@ fn get_command_line_arguments() -> Args {
     }
     args
 }
-
-fn start_search(args: &Args) {
+fn start_search(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
     let bar = ProgressBar::new_spinner().with_message("Collecting files...");
     bar.enable_steady_tick(Duration::from_millis(100));
     // get the files in the directory
     let folder_path: String = args.path.clone();
     let mut hash_map: HashMap<String, Vec<FileInfo>> = HashMap::new();
-    let _files = get_files_in_directory(args,folder_path, &bar);
+    let _result = get_files_in_directory(args, folder_path, &bar);
+    let _files = match _result {
+        Ok(files) => files,
+        Err(e) => {
+            bar.println(format!("Error: {}", e));
+            return Err(e);
+        }
+    };
     if args.debug {
         bar.println(format!("Found {} files", _files.len()));
     }
@@ -116,11 +129,22 @@ fn start_search(args: &Args) {
         }
     }
     bar.finish_and_clear();
+    Ok(())
 }
 
-fn get_files_in_directory(args: &Args, folder_path: String, bar: &ProgressBar) -> Vec<FileInfo> {
+fn get_files_in_directory(args: &Args, folder_path: String, bar: &ProgressBar) -> Result<Vec<FileInfo>, Box<dyn std::error::Error>> {
     let mut files: Vec<FileInfo> = Vec::new();
     let dir_path = std::path::Path::new(folder_path.as_str());
+    match fs::metadata(folder_path.as_str()) {
+        Ok(metadata) => {
+            if !metadata.is_dir() {
+                return Err(Box::<dyn std::error::Error>::from("The path provided is not a directory"));
+            }
+        }
+        Err(_) => {
+            return Err(Box::from("The path provided is not a directory"));
+        }
+    }
     let total_files = std::fs::read_dir(dir_path).unwrap().count();
     bar.println(format!("Searching in: {} - {} objects", folder_path, total_files));
     for entry in std::fs::read_dir(dir_path).unwrap() {
@@ -135,7 +159,7 @@ fn get_files_in_directory(args: &Args, folder_path: String, bar: &ProgressBar) -
                 continue;
             }
             else {
-                let sub_files = get_files_in_directory(args, path.to_str().unwrap().to_string(), bar);
+                let sub_files = get_files_in_directory(args, path.to_str().unwrap().to_string(), bar)?;
                 files.extend(sub_files);
             }
         }
@@ -189,10 +213,10 @@ fn get_files_in_directory(args: &Args, folder_path: String, bar: &ProgressBar) -
         }
     }
 
-    files
+    Ok(files)
 }
 
-fn get_hash_of_file(file_path: &String, _bar: &ProgressBar) -> String {
+fn get_hash_of_file(file_path: &str, _bar: &ProgressBar) -> String {
     let mut file = std::fs::File::open(file_path).unwrap();
     let mut buffer = Vec::new();
     file.read_to_end(&mut buffer).unwrap();
@@ -204,4 +228,178 @@ fn get_md5_hash(buffer: &Vec<u8>) -> String {
     hasher.update(&buffer);
     let hash = hasher.finalize();
     format!("{:x}", hash)
+}
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_command_line_arguments() {
+        // test default values for cmd args
+        let args = get_command_line_arguments();
+        assert_eq!(args.path, ".");
+        assert_eq!(args.recursive, false);
+        assert_eq!(args.debug, true);
+        assert_eq!(args.include_zero_byte_files, false);
+        assert_eq!(args.dry_run, false);
+        assert_eq!(args.include_hidden_files, false);
+    }
+
+    #[test]
+    fn test_get_files_in_directory() {
+        let args = Args {
+            path: "data".to_string(),
+            recursive: false,
+            debug: false,
+            include_zero_byte_files: false,
+            dry_run: false,
+            include_hidden_files: false,
+        };
+        let files = get_files_in_directory(&args, "data".to_string(), &ProgressBar::new_spinner()).unwrap();
+        assert_eq!(files.len(), 5);
+    }
+
+    #[test]
+    fn test_get_files_in_directory_include_empty() {
+        let args = Args {
+            path: "data".to_string(),
+            recursive: false,
+            debug: false,
+            include_zero_byte_files: true,
+            dry_run: false,
+            include_hidden_files: false,
+        };
+        let files = get_files_in_directory(&args, "data".to_string(), &ProgressBar::new_spinner()).unwrap();
+        assert_eq!(files.len(), 7);
+    }
+
+    #[test]
+    fn test_get_files_in_directory_include_hidden() {
+        let args = Args {
+            path: "data".to_string(),
+            recursive: false,
+            debug: false,
+            include_zero_byte_files: false,
+            dry_run: false,
+            include_hidden_files: true,
+        };
+        let files = get_files_in_directory(&args, "data".to_string(), &ProgressBar::new_spinner()).unwrap();
+        assert_eq!(files.len(), 6);
+    }
+
+    #[test]
+    fn test_get_files_in_directory_include_all_files() {
+        let args = Args {
+            path: "data".to_string(),
+            recursive: false,
+            debug: false,
+            include_zero_byte_files: true,
+            dry_run: false,
+            include_hidden_files: true,
+        };
+        let files = get_files_in_directory(&args, "data".to_string(), &ProgressBar::new_spinner()).unwrap();
+        assert_eq!(files.len(), 8);
+    }
+
+    #[test]
+    fn test_get_files_in_directory_include_recursive() {
+        let args = Args {
+            path: "data".to_string(),
+            recursive: true,
+            debug: false,
+            include_zero_byte_files: false,
+            dry_run: false,
+            include_hidden_files: false,
+        };
+        let files = get_files_in_directory(&args, "data".to_string(), &ProgressBar::new_spinner()).unwrap();
+        assert_eq!(files.len(), 16);
+    }
+
+    #[test]
+    fn test_get_files_in_directory_include_recursive_with_hidden() {
+        let args = Args {
+            path: "data".to_string(),
+            recursive: true,
+            debug: false,
+            include_zero_byte_files: false,
+            dry_run: false,
+            include_hidden_files: true,
+        };
+        let files = get_files_in_directory(&args, "data".to_string(), &ProgressBar::new_spinner()).unwrap();
+        assert_eq!(files.len(), 18);
+    }
+
+    #[test]
+    fn test_get_files_in_directory_bad_path() {
+        let args = Args {
+            path: "badpath!!!".to_string(),
+            recursive: true,
+            debug: false,
+            include_zero_byte_files: false,
+            dry_run: false,
+            include_hidden_files: false,
+        };
+        let result = get_files_in_directory(&args, "badpath!!!".to_string(), &ProgressBar::new_spinner());
+        assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_get_hash_of_file() {
+        let hash = get_hash_of_file("data//testdupe1.txt", &ProgressBar::new_spinner());
+        assert_eq!(hash, "8c91214730e59f67bd46d1855156e762");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_get_hash_of_file_bad_path() {
+        let hash = get_hash_of_file("data//testdupe1-notfound.txt", &ProgressBar::new_spinner());
+        assert_eq!(hash, "8c91214730e59f67bd46d1855156e762");
+    }
+
+    #[test]
+    fn test_get_md5_hash() {
+        let buffer = "Hello, world!".as_bytes().to_vec();
+        let hash = get_md5_hash(&buffer);
+        assert_eq!(hash, "6cd3556deb0da54bca060b4c39479839");
+    }
+
+    #[test]
+    fn test_get_md5_hash_empty() {
+        let buffer = "".as_bytes().to_vec();
+        let hash = get_md5_hash(&buffer);
+        assert_eq!(hash, "d41d8cd98f00b204e9800998ecf8427e");
+    }
+
+    #[test]
+    fn test_start_search() {
+        let args = Args {
+            path: "data".to_string(),
+            recursive: false,
+            debug: false,
+            include_zero_byte_files: false,
+            dry_run: false,
+            include_hidden_files: false,
+        };
+        let result = start_search(&args);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_start_search_bad_path() {
+        let args = Args {
+            path: "data-badpath!!!".to_string(),
+            recursive: false,
+            debug: false,
+            include_zero_byte_files: false,
+            dry_run: false,
+            include_hidden_files: false,
+        };
+        let result = start_search(&args);
+        assert!(result.is_err());
+    }
+
 }
